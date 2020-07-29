ebb072e5d4ab314beb2b13093f0ced66a0458ada
Oliveira <merciayanmact@hotmail.com>
2020-07-29 18:25:50 -0300

    design ajustments

--- src/js/index.js
+++ src/js/index.js
@@ -22,29 +22,35 @@ function createProjectList() {
   const projectsArray = [
     {
       name: "Pikere",
-      desc: "asuhdiaushduia",
+      desc: "Aplicativo desktop para upload de imagens",
       url: "https://github.com/MerciaOliveira/pikere",
       src: "/src/assets/imgs/2.gif",
     },
     {
       name: "Pikere",
       desc: "2",
-      url: "3",
-      src: "/src/assets/imgs/2.gif",
+      url: "#",
+      src: "/src/assets/imgs/1.gif",
     },
     {
       name: "Pikere",
       desc: "2",
-      url: "3",
+      url: "#",
       src: "/src/assets/imgs/2.gif",
     },
   ];
   projectsArray.map(function (project) {
     if (project.name) {
       html.push(`
-      <picture class="code__width-container__project__image-container">
-        <img src="${project.src}" alt="" srcset="${project.src}">
-      </picture>
+      <div class="code__width-container__project__image-container">
+        <div class="code__width-container__project__image-container__image" style="--image: url(${project.src})">
+          <div class="code__width-container__project__image-container__link-container">
+            <a href="${project.url}" target="_blank">Source code</a>
+          </div>
+          <div class="code__width-container__project__image-container__name">${project.name}</div>
+          <div class="code__width-container__project__image-container__desc">${project.desc}</div>
+        </div>
+      </div>
       `);
     }
   });
